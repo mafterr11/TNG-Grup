ffd0e7ee5428d2bd7f3ebbf5cb9a97f58988f5d9
mafterr11 <mafterr11@gmail.com>
2024-01-06 10:33:36 +0200

    .

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,16 @@ import AchievementCounter from "@/components/pages/home/AchievementCounter";
 const Home = () => {
   return (
     <div className='py-[5.7rem] '>
+      <div className="text-center container mt-8 flex flex-col md:hidden">
+        <h1>
+          <span className='text-accent'>TNG</span> Grup
+        </h1>
+        <p className='text-white my-4'>
+          Înțelegem că fiabilitatea este cheia și lucrăm îndeaproape cu clienții
+          noștri pentru a ne asigura că îndeplinim nevoile lor și nu îi lăsăm la
+          greu.
+        </p>
+      </div>
       <HeroCarousel />
       <WhyChooseUs />
       <AchievementCounter />
