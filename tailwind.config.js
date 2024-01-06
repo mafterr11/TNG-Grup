e3a31d09bf9f5eac79e2bfd6a90e08063080faf2
mafterr11 <mafterr11@gmail.com>
2024-01-06 19:58:11 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,20 +67,8 @@ module.exports = {
         },
       },
       animation: {
-        keyframes: {
-          "accordion-down": {
-            from: { height: "0" },
-            to: { height: "var(--radix-accordion-content-height)" },
-          },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
-            to: { height: "0" },
-          },
-        },
-        animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
-        },
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
