3636dc028c405488350859adb9bbed0027c8ac64
mafterr11 <mafterr11@gmail.com>
2024-01-07 02:08:54 +0200

    .

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,13 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 export const links = [
   {
@@ -24,13 +32,81 @@ export const links = [
   },
 ];
 
+const dropdownLinks = [
+  {
+    path: "/servicii#execuție-construcții-civile",
+    name: "Execuție Construcții Civile",
+  },
+  {
+    path: "/servicii#execuție-construcții-industriale",
+    name: "Execuție Construcții Industriale",
+  },
+  {
+    path: "/servicii#execuție-construcții-agricole",
+    name: "Execuție Construcții Agricole",
+  },
+  {
+    path: "/servicii#supervizare-lucrări-de-execuție",
+    name: "Supervizare Lucrări De Execuție",
+  },
+  {
+    path: "/servicii#verificare-calitate-lucrări-în-execuție",
+    name: "Verificare Calitate Lucrări În Execuție",
+  },
+  { path: "/servicii#planificarea-execuției", name: "Planificarea Execuției" },
+  { path: "/servicii#servicii-RTE", name: "Servicii RTE" },
+  { path: "/servicii#dirigenție-de-șantier", name: "Dirigenție De Șantier" },
+];
+
 const Nav = ({ containerStyles, linkStyles }) => {
   const pathname = usePathname();
-  return <nav className={`${containerStyles}`}>
-    {links.map((link, index)=>{
-        return <Link key={index} href={link.path} className={`${linkStyles}`}><span>{link.name}</span></Link>
-    })}
-  </nav>;
+  return (
+    <nav className={`${containerStyles} flex items-center justify-center`}>
+      {links.map((link, index) => {
+        if (link.path === "/servicii") {
+          return (
+            <NavigationMenu key={index}>
+              <NavigationMenuList>
+                <NavigationMenuItem as='div'>
+                  <NavigationMenuTrigger as='span' className={`${linkStyles}`}>
+                    {link.name}
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent as='div'>
+                    {dropdownLinks.map((dropdownLink, dropdownIndex) => (
+                      <Link
+                        key={dropdownIndex}
+                        href={dropdownLink.path}
+                        passHref
+                        legacyBehavior 
+                      >
+                        <NavigationMenuLink>
+                          <div>
+                          <div className='mb-4 w-[16rem] flex  gap-x-[4px] '>
+                            <span className='text-accent'>&#9679;</span>
+                            <div className='hover'>
+                              <span>{dropdownLink.name}</span>
+                            </div>
+                          </div>
+                          <span className="w-full h-2 text-white"></span>
+                          </div>
+                        </NavigationMenuLink>
+                      </Link>
+                    ))}
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          );
+        } else {
+          return (
+            <Link key={index} href={link.path} className={`${linkStyles}`}>
+              <span>{link.name}</span>
+            </Link>
+          );
+        }
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
--- components/ui/navigation-menu.jsx
+++ components/ui/navigation-menu.jsx
@@ -33,7 +33,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md capitalize px-4 py-2 text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-900 focus:bg-slate-100 focus:text-slate-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-slate-100/50 data-[state=open]:bg-slate-100/50 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50 dark:focus:bg-slate-800 dark:focus:text-slate-50 dark:data-[active]:bg-slate-800/50 dark:data-[state=open]:bg-slate-800/50"
+  "group inline-flex h-10 w-max items-center justify-center rounded-md capitalize text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-900 focus:bg-slate-100 focus:text-slate-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-slate-100/50 data-[state=open]:bg-slate-100/50 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50 dark:focus:bg-slate-800 dark:focus:text-slate-50 dark:data-[active]:bg-slate-800/50 dark:data-[state=open]:bg-slate-800/50"
 )
 
 const NavigationMenuTrigger = React.forwardRef(({ className, children, ...props }, ref) => (
@@ -43,7 +43,7 @@ const NavigationMenuTrigger = React.forwardRef(({ className, children, ...props 
     {...props}>
     {children}{""}
     <ChevronDown
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+      className="relative top-[1px] ml-1 h-5 w-5 transition duration-200 group-data-[state=open]:rotate-180"
       aria-hidden="true" />
   </NavigationMenuPrimitive.Trigger>
 ))
@@ -53,7 +53,7 @@ const NavigationMenuContent = React.forwardRef(({ className, ...props }, ref) =>
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={cn(
-      "left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
+      "left-0 top-0 w-full bg-grey p-6 data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
       className
     )}
     {...props} />
@@ -66,7 +66,7 @@ const NavigationMenuViewport = React.forwardRef(({ className, ...props }, ref) =
   <div className={cn("absolute left-0 top-full flex justify-center")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
-        "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-slate-200 bg-white text-slate-950 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)] dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
+        "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-slate-200 text-slate-950 shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)] dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
         className
       )}
       ref={ref}
