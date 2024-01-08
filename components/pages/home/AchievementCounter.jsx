904d717bd416a8d3ea4ae57dc6c3438f49315087
Alexandru Maftei <mafterr11@gmail.com>
2024-01-08 10:09:05 +0200

    .

--- components/pages/home/AchievementCounter.jsx
+++ components/pages/home/AchievementCounter.jsx
@@ -8,75 +8,42 @@ const AchievementCounter = () => {
     const { ref, inView } = useInView({
         triggerOnce: true, // Trigger only once
         threshold: 0.3, // Percentage of the element that must be visible
-      });
+    });
     return (
         <div ref={ref} className="w-full bg-black/60 flex items-center justify-center p-16">
             {/* container */}
-            <div className="flex flex-col xl:flex-row gap-y-12 xl:gap-y-20 items-center justify-center xl:gap-x-40 ">
+            <div className="flex flex-col xl:flex-row gap-y-12 xl:gap-y-20 justify-center xl:gap-x-40 ">
                 {/* Ani exp */}
-                <motion.div
-                    variants={fadeIn('down', 0.2)}
-                    initial="hidden"
-                    whileInView={"show"}
-                    viewport={{ once: true, ammount: 0.4 }}
-                    className="flex flex-col gap-y-4">
-                    <div className="flex items-end text-6xl">
-                        <CountUp start={0} end={inView ? 6 : 0} duration={5} />+
-                        <h1 className="item-end text-accent">Ani</h1>
+
+                <div className='flex-1'>
+                    <div className='text-3xl lg:text-5xl font-extrabold mb-2 flex items-end'>
+                        <CountUp start={0} end={inView ? 30 : 0} duration={5} /> +
+                        <div className='text-lg lg:text-base xl:text-3xl font-bold uppercase tracking-[1px] leading-[1] max-w-[150px] lg:max-w-[150px] xl:max-w-[200px] text-accent'>
+                            Ani
+                        </div>
                     </div>
-                    <h1 className="text-4xl text-accent">Experiența</h1>
-                </motion.div>
-                {/* Achievements */}
-                <div className="flex flex-col xl:flex-row xl:gap-x-20 gap-y-12">
-                    {/* first row */}
-                    <motion.div
-                        variants={fadeIn('down', 0.4)}
-                        initial="hidden"
-                        whileInView={"show"}
-                        viewport={{ once: true, ammount: 0.4 }}
-                        className="flex flex-col gap-y-12 xl:gap-y-20">
-                        <div className="flex flex-col items-center">
-                            <div className="text-4xl">
-                                <CountUp start={0} end={inView ? 51 : 0} duration={5} />+
-                            </div>
-                            <div className="item-end text-accent text-xl font-semibold">
-                                Construcții Civile
-                            </div>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <div className="text-4xl">
-                                <CountUp start={0} end={inView ? 121 : 0} duration={5} />+
-                            </div>
-                            <div className="item-end text-accent text-xl font-semibold">
-                                Construcții Industriale
-                            </div>
-                        </div>
-                    </motion.div>
-                    {/* second row */}
-                    <motion.div
-                        variants={fadeIn('down', 0.6)}
-                        initial="hidden"
-                        whileInView={"show"}
-                        viewport={{ once: true, ammount: 0.4 }}
-                        className="flex flex-col gap-y-12 xl:gap-y-20">
-                        <div className="flex flex-col items-center">
-                            <div className="text-4xl">
-                                <CountUp start={0} end={inView ? 20 : 0} duration={5} />+
-                            </div>
-                            <div className="item-end text-accent text-xl font-semibold">
-                                Angajați Proprii
-                            </div>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <div className="text-4xl">
-                                <CountUp start={0} end={inView ? 23 : 0} duration={5} />+
-                            </div>
-                            <div className="item-end text-accent text-xl font-semibold">
-                                Ficați Copți
-                            </div>
-                        </div>
-                    </motion.div>
+                    <div className='text-lg lg:text-base xl:text-xl font-bold uppercase tracking-[1px] leading-[1] max-w-[150px] lg:max-w-[150px] xl:max-w-[200px] text-accent'>
+                        Experiența
+                    </div>
                 </div>
+                <div className='flex-1'>
+                    <div className='text-3xl lg:text-5xl font-extrabold mb-2'>
+                        <CountUp start={0} end={inView ? 50 : 0} duration={5} /> +
+                    </div>
+                    <div className='text-lg lg:text-base xl:text-xl font-bold uppercase tracking-[1px] leading-[1.4] max-w-[150px] lg:max-w-[150px] xl:max-w-[200px] text-accent'>
+                        Construcții Civile
+                    </div>
+                </div>
+                <div className='flex-1'>
+                    <div className='text-3xl lg:text-5xl font-extrabold mb-2'>
+                        <CountUp start={0} end={inView ? 20 : 0} duration={5} /> +
+                    </div>
+                    <div className='text-lg lg:text-base xl:text-xl font-bold uppercase tracking-[1px] leading-[1.4] max-w-[150px] lg:max-w-[150px] xl:max-w-[200px] text-accent'>
+                        Construcții Industriale
+                    </div>
+                </div>
+
+
 
             </div>
         </div>
