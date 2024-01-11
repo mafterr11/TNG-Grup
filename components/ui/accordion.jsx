6e5be9cadf10d3eaf4028c4d6b0aa2f09bb2201a
mafterr11 <mafterr11@gmail.com>
2024-01-11 21:38:28 +0200

    .

--- components/ui/accordion.jsx
+++ components/ui/accordion.jsx
@@ -9,7 +9,7 @@ import { cn } from "@/lib/utils"
 const Accordion = AccordionPrimitive.Root
 
 const AccordionItem = React.forwardRef(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
+  <AccordionPrimitive.Item ref={ref} className={cn("border-b data-[state=open]:text-accent", className)} {...props} />
 ))
 AccordionItem.displayName = "AccordionItem"
 
