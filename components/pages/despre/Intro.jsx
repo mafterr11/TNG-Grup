9e3277db8940e9d271a6bd0b0dab0d3a69bd4812
Alexandru Maftei <mafterr11@gmail.com>
2024-01-11 13:02:21 +0200

    .

--- components/pages/despre/Intro.jsx
+++ components/pages/despre/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className="mb-12 xl:mb-24">
       <div className="w-full xl:h-[28rem] h-64 relative overflow-hidden">
-        <Image src={p1} alt="imagine despre TNG" fill className="object-cover" />
+        <Image src={p1} alt="imagine despre TNG" fill priority={true} className="object-cover" />
         <div className="absolute inset-0 bg-black/60 border-b border-accent" />
         <div className="absolute inset-0 z-10 flex flex-col items-center justify-center">
           <h2 className="text-accent mb-4">TNG GRUP</h2>
