638b2f7c2658576e4c8eff9755fa44f22365b31f
Alexandru Maftei <mafterr11@gmail.com>
2024-01-11 09:39:22 +0200

    .

--- components/pages/despre/Intro.jsx
+++ components/pages/despre/Intro.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import p1 from '../../../public/despre/3.jpg';
+import p1 from '../../../public/despre/2.jpg';
 
 const Intro = () => {
   return (
     <div className="py-[6rem]"> 
     <div className="w-full h-[28rem] relative overflow-hidden">
       <Image src={p1} alt="imagine despre TNG" fill className="object-cover" />
-      <div className="absolute inset-0 bg-black/70" />
+      <div className="absolute inset-0 bg-black/60" />
       <div className="absolute inset-0 z-50 flex flex-col items-center justify-center">
         <h2 className="text-accent">TNG GRUP</h2>
         <h3 className="font-normal">Afla mai multe despre noi</h3>
