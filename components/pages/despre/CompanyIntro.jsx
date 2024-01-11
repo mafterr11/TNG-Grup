e03eea4c0ef7bb67d50a9d973b055e60324ef0dc
mafterr11 <mafterr11@gmail.com>
2024-01-11 17:35:21 +0200

    .

--- components/pages/despre/CompanyIntro.jsx
+++ components/pages/despre/CompanyIntro.jsx
@@ -1,23 +1,41 @@
+"use client"
 import Image from "next/image";
-import p1 from '../../../public/logo.png';
+import p1 from "../../../public/logo.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
 
 const CompanyIntro = () => {
-    return (
-        <div className="container mx-auto">
-            <div className="flex flex-col xl:flex-row items-center justify-center gap-x-12 gap-y-12 xl:gap-y-0">
-                <div className="w-full xl:w-1/2">
-                    <h3 className="text-accent font-normal">Cine suntem noi</h3>
-                    <span className="border-b-2 border-accent block w-2/5 mb-8 mt-4"></span>
-                    <p>TNG GRUP AG este o societate de Antreprenoriat General, cu capital privat integral romanesc, infiintata in anul 2019 din dorinta de a satisfice cerintele clientilor, prin respectarea termenelor contractuale agreate si a cerintelor de calitate si SSM impuse de legislatia in vigoare.
-                        Calitatea lucrărilor este rezultatul eforturilor unui colectiv tânăr şi bine pregătit, argumentul principal în contractarea lucrărilor fiind garantarea acestora precum şi predarea lucrărilor la termenele stabilite.
-                        Societatea deţine în acest sens personal specializat care poate asigura execuţia tuturor lucrărilor solicitate de către client.</p>
-                </div>
-                <div className="w-full xl:w-[40%] h-42 ">
-                    <Image src={p1} alt="TNG logo" className="rounded-3xl object-cover" />
-                </div>
-            </div>
+  return (
+    <motion.div
+      variants={fadeIn("down", 0.2)}
+      initial='hidden'
+      whileInView={"show"}
+      viewport={{ once: true, ammount: 0.4 }}
+      className='container mx-auto'
+    >
+      <div className='flex flex-col xl:flex-row items-center justify-center gap-x-12 gap-y-12 xl:gap-y-0'>
+        <div className='w-full xl:w-1/2'>
+          <h3 className='text-accent font-normal'>Cine suntem noi</h3>
+          <span className='border-b-2 border-accent block w-2/5 mb-8 mt-4'></span>
+          <p>
+            TNG GRUP AG este o societate de Antreprenoriat General, cu capital
+            privat integral romanesc, infiintata in anul 2019 din dorinta de a
+            satisfice cerintele clientilor, prin respectarea termenelor
+            contractuale agreate si a cerintelor de calitate si SSM impuse de
+            legislatia in vigoare. Calitatea lucrărilor este rezultatul
+            eforturilor unui colectiv tânăr şi bine pregătit, argumentul
+            principal în contractarea lucrărilor fiind garantarea acestora
+            precum şi predarea lucrărilor la termenele stabilite. Societatea
+            deţine în acest sens personal specializat care poate asigura
+            execuţia tuturor lucrărilor solicitate de către client.
+          </p>
         </div>
-    )
-}
+        <div className='w-full xl:w-[40%] h-42 '>
+          <Image src={p1} alt='TNG logo' className='rounded-3xl object-cover' />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default CompanyIntro
+export default CompanyIntro;
--- components/pages/despre/Intro.jsx
+++ components/pages/despre/Intro.jsx
@@ -1,27 +1,52 @@
+"use client";
 import Image from "next/image";
-import p1 from '../../../public/despre/2.jpg';
+import p1 from "../../../public/despre/2.jpg";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../variants";
 
 const Intro = () => {
   return (
-    <div className="mb-12 xl:mb-24">
-      <div className="w-full xl:h-[28rem] h-64 relative overflow-hidden">
-        <Image src={p1} alt="imagine despre TNG" fill priority={true} className="object-cover" />
-        <div className="absolute inset-0 bg-black/60 border-b border-accent" />
-        <div className="absolute inset-0 z-10 flex flex-col gap-y-4 items-center justify-center">
-          <h3 className="text-4xl gap-y-2 text-white/80 flex flex-col xl:flex-row items-start justify-center xl:gap-x-2">
-            <div className="text-accent">T<span className="text-white">he</span></div>
-            <div className="text-accent">N<span className="text-white">ew</span></div>
-            <div className="text-accent">G<span className="text-white">eneration</span></div>
+    <div className='mb-12 xl:mb-24'>
+      <div className='w-full xl:h-[28rem] h-64 relative overflow-hidden'>
+        <Image
+          src={p1}
+          alt='imagine despre TNG'
+          fill
+          priority={true}
+          className='object-cover'
+        />
+        <div className='absolute inset-0 bg-black/60 border-b border-accent' />
+        <motion.div
+          variants={fadeIn("down", 0.2)}
+          initial='hidden'
+          whileInView={"show"}
+          viewport={{ once: true, ammount: 0.4 }}
+          className='absolute inset-0 z-10 flex flex-col gap-y-4 items-center justify-center'
+        >
+          <h3 className='text-4xl gap-y-2 text-white/80 flex flex-col xl:flex-row items-start justify-center xl:gap-x-2'>
+            <div className='text-accent'>
+              T<span className='text-white'>he</span>
+            </div>
+            <div className='text-accent'>
+              N<span className='text-white'>ew</span>
+            </div>
+            <div className='text-accent'>
+              G<span className='text-white'>eneration</span>
+            </div>
           </h3>
-          <h3 className="font-normal text-xl xl:text-2xl text-white/80 flex flex-row items-center justify-center">
-            <div>Inovație<span className="text-accent text-2xl">.</span></div>
-            <div>Durabilitate<span className="text-accent text-2xl">.</span></div>
+          <h3 className='font-normal text-xl xl:text-2xl text-white/80 flex flex-row items-center justify-center'>
+            <div>
+              Inovație<span className='text-accent text-2xl'>.</span>
+            </div>
+            <div>
+              Durabilitate<span className='text-accent text-2xl'>.</span>
+            </div>
             <div>Eficiență</div>
           </h3>
-        </div>
+        </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
