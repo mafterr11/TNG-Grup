0787e744cfa65dcd332ce2259bb4fb49949d06a0
mafterr11 <mafterr11@gmail.com>
2024-01-12 21:43:59 +0200

    .

---
+++ app/portofoliu/[slug]/page.jsx
@@ -0,0 +1,9 @@
+const PortofoliuDetails = ({ slug }) => {
+  return (
+    <div className='py-[20rem] container mx-auto'>
+      <h1>Detalii Portofoliu: {slug}</h1>
+    </div>
+  );
+};
+
+export default PortofoliuDetails;
--- app/portofoliu/page.jsx
+++ app/portofoliu/page.jsx
@@ -1,9 +1,22 @@
-const Portofoliu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import Link from "next/link";
 
-export default Portofoliu
+const Portofoliu = () => {
+  const projects = [
+    { name: "Project One", slug: "project-one" },
+    { name: "Project Two", slug: "project-two" },
+    { name: "Project Three", slug: "project-three" },
+  ];
+
+  return (
+    <div className="py-[20rem] container mx-auto">
+      <h1>Proiectele mele</h1>
+      {projects.map((project) => (
+        <div key={project.slug}>
+          <Link href={`/portofoliu/${project.slug}`}>{project.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Portofoliu;
