21ee80daa6cf06a8ab3e561ea0e11da576a8836b
mafterr11 <mafterr11@gmail.com>
2024-01-16 22:48:36 +0200

    .

--- app/despre/page.jsx
+++ app/despre/page.jsx
@@ -2,7 +2,12 @@ import Intro from "@/components/pages/despre/Intro";
 import CompanyIntro from "@/components/pages/despre/CompanyIntro";
 import Team from "@/components/pages/despre/Team";
 
-
+export const metadata = {
+  title: "Despre TNG GRUP - Viziune și Valori",
+  description:
+    "Aflați mai multe despre istoria și valorile TNG GRUP, un nume de încredere în industria construcțiilor din România.",
+  keywords: "istoria TNG GRUP, valori, construcții România",
+};
 
 const Despre = () => {
   return (
