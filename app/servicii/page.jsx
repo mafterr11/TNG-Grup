e6dde8c656e6579e0d1ce1d1f2c516fc78556a50
mafterr11 <mafterr11@gmail.com>
2024-01-16 22:41:02 +0200

    .

--- app/servicii/page.jsx
+++ app/servicii/page.jsx
@@ -1,12 +1,6 @@
 import { ServiciiContent } from "@/components/pages/servicii/ServiciiContent";
 
-export const metadata = {
-  title: "Servicii de Construcții Oferite de TNG GRUP",
-  description:
-    "Descoperiți gama largă de servicii de construcții oferite de TNG GRUP, adaptate nevoilor clientului.",
-  keywords:
-    "servicii construcții TNG GRUP, soluții personalizate, expertiză în construcții, inovație în construcții, dirigenție de șantier, servicii rte",
-};
+
 const Servicii = () => {
   return (
     <div className='pt-[8rem]'>
