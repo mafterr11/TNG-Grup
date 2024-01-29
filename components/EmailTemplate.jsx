b11ef0aa2b34034f103430edbb6739acb88c8e73
Alexandru Maftei <mafterr11@gmail.com>
2024-01-29 08:53:29 +0200

    .

--- components/EmailTemplate.jsx
+++ components/EmailTemplate.jsx
@@ -11,17 +11,17 @@ export const EmailTemplate = ({
   etapa,
   mesaj,
 }) => (
-  <div> 
+  <div>
     <h1>
       Solicitare noua de oferta - TNG GRUP!
     </h1>
     <p>Nume complet: {nume} {prenume}</p>
     <p>Email: {email}</p>
     <p>Telefon: {telefon}</p>
-    <p>Tipul de constructie: {constructie}</p>
-    <p>In ce judet: {judet}</p>
-    <p>Cand vrem sa incepem: {inceput}</p>
-    <p>Etapa in care suntem: {etapa}</p>
+    <p>Tipul de construcție: {constructie}</p>
+    <p>În ce județ: {judet}</p>
+    <p>Când vrem să începem: {inceput}</p>
+    <p>Etapa în care suntem: {etapa}</p>
     <p>Mesaj: {mesaj}</p>
   </div>
 );
