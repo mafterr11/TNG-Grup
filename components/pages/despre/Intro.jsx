a3501f1767d4533cde1fc8a6fe47eb70fcfe3723
Alexandru Maftei <mafterr11@gmail.com>
2024-02-02 16:13:36 +0200

    .

--- components/pages/despre/Intro.jsx
+++ components/pages/despre/Intro.jsx
@@ -8,40 +8,40 @@ import { fadeIn } from "../../../variants";
 
 const Intro = () => {
   return (
-    <div className='mb-12 xl:mb-24'>
-      <div className='w-full xl:h-[28rem] h-64 relative overflow-hidden'>
+    <div className="mb-12 xl:mb-24">
+      <div className="w-full xl:h-[28rem] h-64 relative overflow-hidden">
         <Image
           src={p1}
-          alt='Despre TNG imagine'
+          alt="Despre TNG imagine"
           fill
           priority={true}
-          className='object-cover'
+          className="object-cover"
         />
-        <div className='absolute inset-0 bg-black/60 border-b border-accent' />
+        <div className="absolute inset-0 bg-black/60 border-b border-accent" />
         <motion.div
           variants={fadeIn("down", 0.2)}
-          initial='hidden'
+          initial="hidden"
           whileInView={"show"}
           viewport={{ once: true, ammount: 0.4 }}
-          className='absolute inset-0 z-10 flex flex-col gap-y-4 items-center justify-center'
+          className="absolute inset-0 z-10 flex flex-col gap-y-4 items-center justify-center"
         >
-          <div className='text-4xl font-bold gap-y-2 text-white/80 flex flex-col xl:flex-row items-start justify-center xl:gap-x-2'>
-            <div className='text-accent'>
-              T<span className='text-white'>he</span>
+          <div className="text-4xl font-bold gap-y-2 text-white/80 flex flex-col xl:flex-row items-start justify-center xl:gap-x-2">
+            <div className="text-white">
+              <span className="text-accent">T</span>he
             </div>
-            <div className='text-accent'>
-              N<span className='text-white'>ew</span>
+            <div className="text-white">
+              <span className="text-accent">N</span>ew
             </div>
-            <div className='text-accent'>
-              G<span className='text-white'>eneration</span>
+            <div className="text-white">
+              <span className="text-accent">G</span>eneration
             </div>
           </div>
-          <h3 className='flex flex-row items-center justify-center font-normal text-xl xl:text-2xl text-white/80'>
+          <h3 className="flex flex-row items-center justify-center font-normal text-xl xl:text-2xl text-white/80">
             <div>
-              Inovație<span className='text-accent text-2xl'>.</span>
+              Inovație<span className="text-accent text-2xl">.</span>
             </div>
             <div>
-              Durabilitate<span className='text-accent text-2xl'>.</span>
+              Durabilitate<span className="text-accent text-2xl">.</span>
             </div>
             <div>Eficiență</div>
           </h3>
