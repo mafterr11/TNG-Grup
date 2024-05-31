d099975e15672ef68a8bbe0ca1b59febe87b0d48
mafterr11 <mafterr11@gmail.com>
2024-06-01 02:29:00 +0300

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,8 @@ module.exports = {
   siteUrl:
     process.env.SITE_URL ||
     "http://localhost:3000" ||
-    "https://tng-grup.vercel.app",
+    "https://tng-grup.vercel.app" ||
+    "https://tngag.ro",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
