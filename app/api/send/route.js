86cc601d6e016021f9f67fde3323ed075388b6b9
mafterr11 <mafterr11@gmail.com>
2024-06-16 17:55:59 +0300

    .

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -9,7 +9,7 @@ export async function POST(request) {
 
     const data = await resend.emails.send({
       from: "TNGAG <solicitari@tngag.ro>",
-      to: ["mafterr11@gmail.com"],
+      to: ["office@tngag.ro"],
       subject: "TNG Solicitare Nouă",
       react: EmailTemplate(formData), // pass the entire formData object to the template
     });
